cf29f8096ffbde1df22793cad2c4111f842dd6c8
Frank-Jing <jingwx36@gmail.com>
2017-11-13 01:44:20 -0800

    Add code to week4

--- Practice/week4/week4_ppt_code.R
+++ Practice/week4/week4_ppt_code.R
@@ -33,16 +33,55 @@ mydate <- as.Date(mychar, format = "%B %eth, %Y")
 # format	A character string.
 # If not specified, it will try "%Y-%m-%d" then "%Y/%m/%d" on the first non-NA element, and give an error if neither works.
 
+# extract parts from date
+mydate <- as.Date("2017-10-05")
+weekdays(mydate)
+months(mydate)
+quarters(mydate)
+
+# Difference between 2 dates
+time1 <- as.Date("2017-10-05")
+time2 <- as.Date("2008-07-08")
+time1 - time2
+difftime(time1, time2, units = "weeks")
+difftime(time1, time2, units = "days") # the same as time1-time2
+
 ### Generate Regular Sequences of Dates
 ## first days of years
 seq(as.Date("2007/1/1"), as.Date("2010/1/1"), "years")
 ## by month
 seq(as.Date("2000/1/1"), by = "month", length.out = 4)
 ## quarters
-seq(as.Date("2000/1/1"), as.Date("2001/1/1"), 
-    by = "quarter")
+seq(as.Date("2000/1/1"), as.Date("2001/1/1"), by = "quarter")
 
-## XTS
+## package "lubridate"
+library(lubridate)
+ymd("20110604") 
+mdy("06-04-2011")
+dmy("04/06/2011")
+# above all transform the date into form "yyyy-mm-dd"
+
+arrive <- ymd_hms("2011-06-04 12:00:25")
+second(arrive)
+# !! seconds(arrive) is not the same as second(arrive), instead it computes seconds from 1970-12-31
+second(arrive) <- 45
+arrive  # here the second converts to "45" from "25"
+
+mydate <- ymd("20130130")
+mydate + days(2)
+mydate + months(5)
+
+mydate <- ymd("20130130")
+mydate + days(1:5) # return a vector of char
+# "2013-01-31" "2013-02-01" "2013-02-02" "2013-02-03" "2013-02-04"
+
+jan31 <- ymd("2013-01-31")
+jan31 + months(1) # NA because Feb has no 31th
+ceiling_date(jan31, "month") - days(1)  # returns 2013-01-31
+# Rounding to the nearest unit or multiple of a unit are supported
+floor_date(jan31, "month") + months(2) - days(1) #returns 2013-02-28
+
+## package "XTS"
 ### Construction
 x <- matrix(1:4, nrow=2, ncol=2)
 idx <- seq(as.Date("2016-10-27"), length=2, by="days")
@@ -53,6 +92,13 @@ x_xts
 coredata(x_xts)  # Gives us a martix
 index(x_xts)  # Gives us a vector of dates
 
+## extract
+edhec[c(1,2), 1]
+edhec[(index(edhec) <= "1997-02-28"), 1]
+edhec[c("1997-01-31","1997-02-28") , 1]
+# above have the same results
+
+
 ### first()
 first(edhec[, "Convertible Arbitrage" ], "3 months")
 
@@ -60,7 +106,30 @@ first(edhec[, "Convertible Arbitrage" ], "3 months")
 x <- edhec["199701/02", 1]
 y <- edhec["199702/03", 1]
 z <- edhec["199703/04", 1]
-x + y # only the intersection
+x + y # only the intersection value adding together
+
+merge(y, index(x), fill = 0)         # replace the missmatched part by 0
+#            Convertible.Arbitrage
+# 1997-01-31                0.0000
+# 1997-02-28                0.0123
+# 1997-03-31                0.0078
+x + merge(y, index(x), fill = 0)
+#            Convertible.Arbitrage
+# 1997-01-31                0.0119
+# 1997-02-28                0.0246  <-  x+y
+
+merge(y, index(x), fill = na.locf)   # replace the missmatched part by NA
+#            Convertible.Arbitrage
+# 1997-01-31                    NA
+# 1997-02-28                0.0123
+# 1997-03-31                0.0078
+
+x + merge(y, index(x), fill = na.locf)
+#            Convertible.Arbitrage
+# 1997-01-31                    NA  <- NA + x  = NA
+# 1997-02-28                0.0246
+
+
 
 
 
